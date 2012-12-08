7607b6c7e4fc8157fa36f2c43166e60c319c62e4
tka.lu <tkatka@gmail.com>
2012-12-09 02:06:20 +0800

    增加201201-201211的決標公告url

---
+++ ruby-pcc-parser/README.markdown
@@ -0,0 +1,21 @@
+# 取得特定日期決標案網址 
+
+`get_tender_urls_by_date.rb STARTDATE [ENDDATE]`
+
+取得 2012-01-01 的決標案
+`get_tender_urls_by_date 2012-01-01`
+
+取得 2012-01-01 ~ 2012-01-31的決標案
+`get_tender_urls_by_date 2012-01-01 2012-01-31`
+
+執行後會下載到 `tender-urls` 目錄中
+
+# 下載標案網頁
+
+tender_page_downloader.rb
+
+執行後會下載由 `get_tender_urls_by_date.rb` 產生 `tender-urls` 裏面的 url 儲存到 `tender-pages` 目錄中
+
+# 將決標網頁轉成 json 
+
+`parser.rb TARGET_FOLDER`
---
+++ ruby-pcc-parser/tender_page_downloader.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'open-uri'
+require 'fileutils'
+base_dir = 'tender-pages'
+
+FileUtils.mkdir_p('base_dir')
+Dir.glob('tender-urls/*.json').sort.each do |path|
+  date = path.match(/\d{4}-\d{2}-\d{2}/)[0]
+  html_dir = File.join(base_dir, date)
+  FileUtils.mkdir_p(html_dir)
+
+  urls = JSON.load(open(path))
+  urls.each_with_index do |url, index|
+    if url =~ /pkAtmMain=(\d+)/
+      html_path = File.join(html_dir, $1) 
+      puts "get: #{date} - #{index+1}/#{urls.length} - #{$1} - #{url}"
+      if !File.exists? html_path
+        open(html_path,'w'){|f| f.write(open(url))} 
+      end
+    end
+  end
+
+end
